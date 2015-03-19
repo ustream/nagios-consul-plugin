8340690f60fe61db2b4e4b10e622457b39d1ee03
pepov <pepov@ustream.tv>
2015-03-19 12:50:21 +0100

    initial

---
+++ README.md
@@ -0,0 +1,44 @@
+## What is this?
+
+This is a consul check integration plugin for nagios that makes easy to add nagios commands that queries specific checks from a consul agent.
+
+Returns 2 if there are any critical checks, 1 if there are no criticals but warnings. Returns 3 when node not found or when there is no matches based on the given filters (CheckID, ServiceName). Returns 0 on passing checks.
+
+## Example
+
+Query the local consul agent for the serf health check of node named consul01 in datacenter dc01:
+```
+$ python check-consul-health.py node consul01 dc01 --CheckID=serfHealth
+Passing: 1
+> consul01::Serf Health Status:serfHealth:passing
+```
+
+
+## Install dependencies with pip
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+```
+$ python check-consul-health.py -h
+Usage: 
+    check-consul-health.py node NODE DC
+        [--addr=ADDR]
+        [--CheckID=CheckID | --ServiceName=ServiceName]
+        [--verbose]
+
+Arguments:
+    NODE  the consul node_name
+    DC    the consul datacenter
+
+Options:
+    -h --help                  show this
+    -v --verbose               verbose output
+    --addr=ADDR                consul address [default: http://localhost:8500]
+    --CheckID=CheckID          CheckID matcher
+    --ServiceName=ServiceName  ServiceName matcher
+```
+
---
+++ check-consul-health.py
@@ -0,0 +1,80 @@
+"""Usage: 
+    check-consul-health.py node NODE DC
+        [--addr=ADDR]
+        [--CheckID=CheckID | --ServiceName=ServiceName]
+        [--verbose]
+
+Arguments:
+    NODE  the consul node_name
+    DC    the consul datacenter
+
+Options:
+    -h --help                  show this
+    -v --verbose               verbose output
+    --addr=ADDR                consul address [default: http://localhost:8500]
+    --CheckID=CheckID          CheckID matcher
+    --ServiceName=ServiceName  ServiceName matcher
+"""
+
+from docopt import docopt
+import requests, json
+
+def dump(it):
+    if arguments['--verbose']: print it
+
+def buildNodeUrl():
+    url = "%(--addr)s/v1/health/node/%(NODE)s?dc=%(DC)s" % arguments
+    dump("Url: " + url)
+    return url
+
+def getJsonFromUrl(url):
+    r = requests.get(url)
+    dump("Response: " + r.text)
+    return r.json()
+
+def printCheck(check):
+    print "> %(Node)s:%(ServiceName)s:%(Name)s:%(CheckID)s:%(Status)s" % check
+
+def processFailing(checks):
+    filters = map(lambda field: \
+        lambda x: arguments['--' + field] is None or x[field] == arguments['--'+field],
+        ['CheckID', 'ServiceName']
+    )
+
+    filtered = filter(lambda x: all(f(x) for f in filters), checks)
+    passing  = filter(lambda x: x['Status'] == 'passing', filtered)
+    warning  = filter(lambda x: x['Status'] == 'warning', filtered)
+    critical = filter(lambda x: x['Status'] == 'critical', filtered)
+
+    if len(checks) == 0:
+        print "There is no matching node"
+        return 3
+
+    if len(filtered) == 0:
+        print "There is no matching checks"
+        return 3
+
+    namesOfChecks = lambda x: x["ServiceID"] + ":" + x["Name"]
+
+    if len(critical):
+        print "Critical: %d (%s)" % (len(critical), ",".join(map(namesOfChecks, critical)))
+        for check in critical:
+            printCheck(check)
+    if len(warning):
+        print "Warning: %d (%s)" % (len(warning), ",".join(map(namesOfChecks, warning)))
+        for check in warning:
+            printCheck(check)
+    if len(passing):
+        print "Passing: %d" % (len(passing))
+        for check in passing:
+            printCheck(check)
+
+    return 2 if len(critical) else 1 if len(warning) else 0
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    dump("Arguments: " + str(arguments))
+    if arguments['node']:
+        url = buildNodeUrl()
+        json = getJsonFromUrl(url)
+        exit(processFailing(json))
